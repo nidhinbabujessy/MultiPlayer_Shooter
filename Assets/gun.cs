8be118e5c2765af4ffaf33475daa643dc9f17324
nidhinbabujessy <118510554+nidhinbabujessy@users.noreply.github.com>
2023-08-11 10:26:55 +0530

    shoot target

--- Assets/gun.cs
+++ Assets/gun.cs
@@ -6,29 +6,30 @@ public class gun : MonoBehaviour
     public Camera fpsCam;
     public float damage = 10f;
     public float range = 100f;
+    public ParticleSystem mucleFlash;
+    public GameObject fireEffect;
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) // Change "Fire1" to the name of your button input
-        {
-            Shoot();
-        }
     }
 
 
     public void Shoot()
     {
+        mucleFlash.Play();
     
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range ))
             {
             EnemyDie enemyDie = hit.transform.GetComponent<EnemyDie>();
             {
+                
                 if(enemyDie != null)
                 {
                     enemyDie.TakeDamage(damage);
                 }
             }
+            Instantiate(fireEffect,hit.point,Quaternion.LookRotation(hit.normal));
         }
     }
 }
